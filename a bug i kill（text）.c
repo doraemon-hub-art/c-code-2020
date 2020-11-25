db38d947dfc6c45b4e16e1cfaef886bba127626a
doraemon-hub-art <74894377+doraemon-hub-art@users.noreply.github.com>
2020-11-25 23:41:38 +0800

    a text to explain the trouble bug(2020.11.25)

---
+++ a bug i kill（text）.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(void)
+{
+	//定义字符变量
+	char prove;
+	//数组 字符串
+	char password[20] = { 0 };
+	printf("请输入密码:");
+	//数组本身就是地址不用加&
+	scanf("%s", password);
+	//回收\n防止他干预下面的接收 
+	//在代码运行窗口输入的回车也会被当做字符而被获取
+	getchar();
+	printf("请确认密码Y/N:");
+	prove = getchar();
+	if (prove == 'Y')
+		printf("确认成功");
+	else
+		printf("确认失败");
+	return 0;
+}
