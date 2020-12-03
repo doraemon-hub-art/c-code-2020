61f32e6dec16b1cefcc780b8321d916ac2ccf455
doraemon-hub-art <74894377+doraemon-hub-art@users.noreply.github.com>
2020-12-03 15:41:54 +0800

    least common dicisor

--- five number's  average.c
+++ five number's  average.c
@@ -1,15 +1,15 @@
 //辗转相除法求两个数的最大公约数
-#incldue<stdio.h>
-int main(void)
-{
-	int r, n, m;
-	printf("输入两个大于零的整数：");
-	scanf_s("%d %d", &m, &n);
-	while (m%n)
-	{
-		m = n;
-		n = r;
-	}
-	printf("%d\n", n);
-	return 0;
-}
+//#incldue<stdio.h>
+//int main(void)
+//{
+//	int r, n, m;
+//	printf("输入两个大于零的整数：");
+//	scanf_s("%d %d", &m, &n);
+//	while (m%n)
+//	{
+//		m = n;
+//		n = r;
+//	}
+//	printf("%d\n", n);
+//	return 0;
+//}
